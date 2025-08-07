3ffb2a7852f320203ebfaa137c354f20ca2da55b
Harini Manohar <harini.manohar2@gmail.com>
2025-08-07 10:58:29 -0230

    Spring Boot setup and app running

---
+++ src/main/java/com/harini/DSA/DsaFinalSprintApplication.java
@@ -0,0 +1,13 @@
+package com.harini.DSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaFinalSprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaFinalSprintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harini/DSA/DsaFinalSprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harini.DSA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaFinalSprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
